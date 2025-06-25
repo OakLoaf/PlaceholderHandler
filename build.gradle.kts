75a047a72301e2a2acf6537e35b4916fb5b4d092
OakLoaf <oak@beaconstudios.org>
2025-06-25 17:21:06 +0100

    Added missing repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ allprojects {
         mavenCentral()
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://repo.papermc.io/repository/maven-public/") // Paper
+        maven("https://repo.helpch.at/releases/") // PlaceholderAPI
     }
 
     tasks {
